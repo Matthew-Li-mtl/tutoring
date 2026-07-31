f4b0b3e808b327efd44136fa1ccdb111ae6ff287
Matthew Li <matthewli.mtl@gmail.com>
2026-07-30 18:10:07 -0700

    plugin-react

--- tutoring-website/tutoring-personal-site/vite.config.ts
+++ tutoring-website/tutoring-personal-site/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+//import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react(), tailwindcss(),],
-  root: path.resolve(__dirname, 'tutoring website/tutoring-personal-site')
+  //root: path.resolve(__dirname, 'tutoring website/tutoring-personal-site')
 })
